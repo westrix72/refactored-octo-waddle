d2f373542f548e2d9559d53c7b21c08dbfea9c65
westrix72 <lfeliperebello@live.com>
2021-09-01 20:12:13 -0300

    Aplicanto layouts para activities

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -2,15 +2,31 @@ package com.example.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Teste", Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Alex", "Fran", "Jose", "Felipe"));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                alunos));
     }
 }
